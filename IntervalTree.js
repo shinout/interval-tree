23e7f80b8ed3be1fdcced15c15444f42b3b38a7b
kennethsabir <kensab@garvan.org.au>
2014-01-31 23:26:09 +1100

    changed references to SortedList.arr to use toArray as arr is now hidden

--- IntervalTree.js
+++ IntervalTree.js
@@ -133,7 +133,7 @@ function _pointSearch(node, idx, arr) {
   if (!node) return;
 
   if (idx < node.idx) {
-    node.starts.arr.every(function(itvl) {
+    node.starts.toArray().every(function(itvl) {
       var bool = (itvl.start <= idx);
       if (bool) arr.push(itvl.result());
       return bool;
@@ -142,7 +142,7 @@ function _pointSearch(node, idx, arr) {
   }
 
   else if (idx > node.idx) {
-    node.ends.arr.every(function(itvl) {
+    node.ends.toArray().every(function(itvl) {
       var bool = (itvl.end >= idx);
       if (bool) arr.push(itvl.result());
       return bool;
@@ -151,7 +151,7 @@ function _pointSearch(node, idx, arr) {
   }
   // exact equal
   else {
-    node.starts.arr.map(function(itvl) { arr.push(itvl.result()) });
+    node.starts.toArray().map(function(itvl) { arr.push(itvl.result()) });
   }
 }
 
@@ -178,19 +178,19 @@ function _rangeSearch(start, end, arr) {
 
 
   var idx1 = this.pointTree.bsearch([start, null]);
-  while (idx1 >= 0 && this.pointTree.arr[idx1][0] == start) {
+  while (idx1 >= 0 && this.pointTree.toArray()[idx1][0] == start) {
     idx1--;
   }
 
   var idx2 = this.pointTree.bsearch([end,   null]);
   if (idx2 >= 0)
   {
-    var len = this.pointTree.arr.length -1;
-    while (idx2 <= len && this.pointTree.arr[idx2][0] <= end) {
+    var len = this.pointTree.toArray().length -1;
+    while (idx2 <= len && this.pointTree.toArray()[idx2][0] <= end) {
       idx2++;
     }
 
-    this.pointTree.arr.slice(idx1 + 1, idx2).forEach(function(point) {
+    this.pointTree.toArray().slice(idx1 + 1, idx2).forEach(function(point) {
       var id = point[1];
       resultHash[id] = true;
     }, this);
